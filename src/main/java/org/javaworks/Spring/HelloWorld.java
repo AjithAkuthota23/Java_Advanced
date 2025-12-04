fdbd1339fed019f194250599c0be62b914a3f92d
AjithAkuthota23 <ajithakuthota@gmail.com>
2025-12-04 14:46:34 -0600

    Spring Basics-Hello world and DI

---
+++ src/main/java/org/javaworks/Spring/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.javaworks.Spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
